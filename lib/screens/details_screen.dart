059afd1440373819dee06d693166c085f6576ed5
jerlibgnzlz <jerlibgv@gmail.com>
2023-08-03 03:21:43 -0300

    add custom appbar

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -2,12 +2,48 @@ import 'package:flutter/material.dart';
 
 class DetailsScreen extends StatelessWidget {
   const DetailsScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Details Screen"),
-      ),
+    // TODO: cambiar luego por una instancia
+    final String movie =
+        ModalRoute.of(context)?.settings.arguments.toString() ?? "no-movie";
+
+    return Scaffold(
+      body: CustomScrollView(slivers: [
+        const SliverAppBar(
+          backgroundColor: Colors.indigo,
+          expandedHeight: 200,
+          floating: false,
+          pinned: true,
+          flexibleSpace: FlexibleSpaceBar(
+            centerTitle: true,
+            titlePadding: EdgeInsets.all(0),
+            title: Text("movie.title"),
+            background: FadeInImage(
+              placeholder: AssetImage("assets/loading.gif"),
+              image: NetworkImage("https://via.placeholder.com/500x300"),
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+        SliverList(
+            delegate: SliverChildBuilderDelegate(
+          (context, index) => const Text(""),
+        ))
+      ]),
     );
   }
 }
+
+// class _CustomAppBar extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     // ignore: prefer_const_constructors
+//     return SliverAppBar(
+//       backgroundColor: Colors.indigo,
+//       // expandedHeight: 200,
+//       // title: const Text('sliver'),
+//     );
+//   }
+// }
